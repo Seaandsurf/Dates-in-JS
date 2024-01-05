ca2024068aa1a27cf89cdf6b6bfc350efda00e2f
Seaandsurf <mathias.hardat@gmail.com>
2024-01-05 16:25:56 +0100

    exercice-4

--- ex-4/scripts/script.js
+++ ex-4/scripts/script.js
@@ -2,10 +2,17 @@ let is24HourFormat = true;
   
 function updateClock() {
   const now = new Date();
-  document.getElementById('day').textContent = now.toLocaleString('en-us', {weekday: 'short'});
+  document.getElementById('day').textContent = now.toLocaleString('fr-FR', {weekday: 'short'}).toUpperCase();
   document.getElementById('date').textContent = now.getDate().toString();
-  document.getElementById('month').textContent = now.toLocaleString('en-us', {month: 'short'});
+  document.getElementById('month').textContent = now.toLocaleString('fr-FR', {month: 'short'}).toUpperCase();
   document.getElementById('year').textContent = now.getFullYear();
   document.getElementById('time').textContent = now.toLocaleTimeString('en-us', {hour12: !is24HourFormat});
 }
 
+function toggleFormat() {
+    is24HourFormat = !is24HourFormat;
+    updateClock();
+  }
+  
+  setInterval(updateClock, 1000);
+  updateClock();
